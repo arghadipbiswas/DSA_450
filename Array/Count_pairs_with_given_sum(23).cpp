6fd3f96a2f0bc5446c7956a4f26691babd682946
Arghadip <arghadipbiswas4076@gmail.com>
2022-11-18 17:35:39 +0530

    added files

---
+++ Array/Count_pairs_with_given_sum(23).cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+    unordered_map<int,int> m;
+    int cnt = 0;
+    for(int i= 0 ; i<n ; i++)
+    {
+        if(k - arr[i] > 0)
+        {
+             cnt += m[k - arr[i]];
+             
+            m[arr[i]]++;
+        }
+    }
+    return cnt;
+    }
+    
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
